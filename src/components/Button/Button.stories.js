892690ae2580e1d26cfd97dcf90461081f42e71f
karinfdez <karinfdez@yahoo.com>
2021-05-14 17:15:41 -0400

    Add args to the stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -6,7 +6,28 @@ export default {
     component: Button,
 };
 
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
+const Template = (args) => <Button {...args} />;
+export const Primary = Template.bind({});
+export const Secondary = Template.bind({});
+export const Success = Template.bind({});
+export const Danger = Template.bind({});
+
+Primary.args = {
+    variant: "primary",
+    children: "Primary",
+};
+
+Secondary.args = {
+    variant: "secondary",
+    children: "Secondary",
+};
+
+Success.args = {
+    variant: "success",
+    children: "Success",
+};
+
+Danger.args = {
+    variant: "danger",
+    children: "Danger",
+};
