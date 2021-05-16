e6c568f5d0e08b432b5a880aad4a14448cc36127
karinfdez <karinfdez@yahoo.com>
2021-05-16 12:20:54 -0400

    remove unnecessary import

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Button.css";
-import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 
 function Button(props) {
     const { variant = "primary", children, ...rest } = props;
